d43785a6d084bd7923be9275a672eafa5f1a6f07
sidrusiya <51047433+sidrusiya@users.noreply.github.com>
2021-01-30 16:59:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const port=process.env.PORT || 9000;
 //middleware
 app.use(express.json());
 
-//DB Connection
-const uri = "mongodb+srv://sidrusiya:Sidrusiya1@cluster0.xdkvg.mongodb.net/Production?retryWrites=true&w=majority";
+//DB Connection and  remove password due to security reason.
+const uri = "mongodb+srv://sidrusiya:<password>@cluster0.xdkvg.mongodb.net/Production?retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useCreateIndex: true,
     useNewUrlParser: true,
